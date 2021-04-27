4fb289ccc7939ae91edb55a48cd9656f0af03c16
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-04-27 18:17:00 -0300

    adiciona mais exercicios dia 4.4

--- bloco_4/dia_4/script.js
+++ bloco_4/dia_4/script.js
@@ -49,3 +49,46 @@ let names = {
   for(let index in carro) {
       console.log(index, ':', carro[index]);
   }
+
+  //Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+  function soma(a, b) {
+      return a + b;
+  }
+
+  function subtracao(a, b) {
+      return a - b;
+  }
+
+  function divisao(a, b) {
+      return a / b;
+  }
+
+  function multiplicacao(a, b) {
+      return a * b;
+  }
+
+  function modulo(a, b) {
+      return a % b;
+  }
+
+  //Faça um programa que retorne o maior de dois números. 
+  function maiorNumero(a, b) {
+      if (a > b) {
+          return a;
+      } else {
+          return b;
+      }
+  }
+
+  //Faça um programa que retorne o maior de três números. 
+
+  function maiorNumero(a, b, c) {
+      if (a > b && a > c) {
+          return a;
+      } else if (b > a && b > c) {
+          return b;
+      } else {
+          return c;
+      }
+  }
