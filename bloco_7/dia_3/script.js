5f34b481c635e4350ededb37d0478dee8af1c6f6
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-21 23:39:22 -0300

    Realiza primeiro e segundo exercicio dia 7.2

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -2,3 +2,72 @@ const assert = require('assert'); // Sintaxe para incluir o módulo assert
 
 assert.strictEqual(50, 50); // Sem erros: 50 == 50
 assert.strictEqual(50, 70); // AssertionError: 50 == 70
+
+// /Neste outro exemplo utilizamos o assert para testar o retorno esperado da função division :
+
+const assert = require('assert');
+
+function division(x, y) {
+  return x / y;
+}
+
+const expected = division(9, 3);
+
+assert.strictEqual(expected, 3, 'Nove dividido por três é igual a três');
+
+// /É possível também testar estruturas como arrays e objetos:
+
+const assert = require('assert');
+
+const list1 = [1, 2, 3, 4, 5];
+const list2 = [1, 2, 3, 4, 5];
+
+assert.deepStrictEqual(list1, list2, 'Erro: list1 e list2 não são estritamente iguais');
+
+const person1 = { name: 'john', age: 21 };
+const person2 = { name: 'john', age: 21 };
+
+assert.deepStrictEqual(person1, person2, 'Erro: person1 e person2 não são estritamente iguais');
+
+const person3 = { name: 'john', age: 19 };
+
+assert.notDeepStrictEqual(person1, person3, 'Erro: os valores dos objetos são estritamente iguais');
+
+//exercises
+
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => {
+    sum(4, '5');
+})
+
+//
+
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+let meuArr = [1, 2, 3, 4];
+myRemove(meuArr, 5);
+assert.deepStrictEqual(myRemove(meuArr, 5) [1, 2, 3, 4]);
