ca03daf706fe9b060d4cb03de02a5198cb59eb80
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-26 16:11:52 -0300

    Começando exercicios dia 8.2

--- bloco_8/dia_2/script.js
+++ bloco_8/dia_2/script.js
@@ -84,3 +84,17 @@ const people = [
   people.sort((a, b) => a.age - b.age);
   
 console.log(people);
+
+//------------------------------------------//
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+];
+  
+  people.sort((a, b) => b.age - a.age);
+  
+console.log(people);
