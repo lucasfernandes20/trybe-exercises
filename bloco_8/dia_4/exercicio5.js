b6603d956193c4e0f7816506c15c6b38146e5808
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-30 20:20:57 -0300

    Resolve exercicio 5

---
+++ bloco_8/dia_4/exercicio5.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  return names.reduce((acc, curr) =>
+  acc + curr.split('').reduce((acc2, curr2) => {
+      if(curr2 === 'a' || curr2 === 'A') return acc2 + 1
+      return acc2;
+  }, 0), 0)
+}
+
+assert.deepStrictEqual(containsA(), 20);
