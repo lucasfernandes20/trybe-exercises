131f821384250de4fde7234c72ffc428a9f51eef
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-06-15 19:43:31 -0300

    Realiza requisito 1 em um unico arquivo

--- bloco_10/dia_2/projetos/upperCase.test.js
+++ bloco_10/dia_2/projetos/upperCase.test.js
@@ -1,10 +1,10 @@
 const uppercase = (str, callback) => {
     callback(str.toUpperCase());
-  }
+}
   
-  it(`uppercase 'test' to equal 'TEST'`, (done) => {
+it(`uppercase 'test' to equal 'TEST'`, (done) => {
     uppercase('test', (str) => {
-      expect(str).toBe('TEST');
-      done();
+        expect(str).toBe('TEST');
+        done();
     });
-  });
+});
